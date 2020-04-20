cda1e7b96fb71b95b972eedda5c05afe5658d05d
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-04-21 02:18:08 +0900

    Create 02446.cpp

---
+++ 02446.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+
+	for (int i = N; i >= 1; i--) {
+		for (int j = 0; j < N - i; j++)
+			cout << ' ';
+		for (int j = 0; j < i * 2 - 1; j++)
+			cout << '*';
+		cout << endl;
+	}
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < N - i; j++)
+			cout << ' ';
+		for (int j = 0; j < i * 2 - 1; j++)
+			cout << '*';
+		cout << endl;
+	}
+}
