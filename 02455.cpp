917a7288d0351629070fd6e5ab47ea6af502e3d9
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-04-21 02:22:27 +0900

    Create 02455.cpp

---
+++ 02455.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int people = 0, max = 0;
+	for (int i = 0; i < 4; i++) {
+		int on, off; cin >> off >> on;
+
+		people -= off;
+		people += on;
+
+		if (people > max) max = people;
+	}
+
+	cout << max << endl;
+}
