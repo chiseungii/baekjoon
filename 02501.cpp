a4ebc81a0744510ea03899f0068e56dd7ae36a0c
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-04-21 22:08:28 +0900

    Create 02501.cpp

---
+++ 02501.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, K; cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		if (N % i == 0) K--;
+		if (K == 0) {
+			cout << i << endl;
+			break;
+		}
+	}
+
+	if (K > 0) cout << 0 << endl;
+}
