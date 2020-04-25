f17dd166a6b37b31ab6fbaffbf296669582476e9
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-04-26 01:43:51 +0900

    Create 04504.cpp

---
+++ 04504.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	while (1) {
+		int a; cin >> a;
+		if (a == 0) break;
+
+		if (a % n)
+			cout << a << " is NOT a multiple of " << n << ".\n";
+		else
+			cout << a << " is a multiple of " << n << ".\n";
+	}
+}
