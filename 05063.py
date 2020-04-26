9e28cf274bd8698c376e6020b33a69c03c6d5e44
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-04-27 02:17:01 +0900

    Create 05063.py

---
+++ 05063.py
@@ -0,0 +1,9 @@
+for i in range(int(input())):
+    r, e, c = map(int, input().split())
+
+    if e - c == r:
+        print("does not matter")
+    elif e - c > r:
+        print("advertise")
+    else:
+        print("do not advertise")
