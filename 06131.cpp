51dcc868098b741443ad6da8d6e1a45c5ba439d5
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-04-30 03:33:02 +0900

    Create 06131.cpp

---
+++ 06131.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N; cin >> N;
+	int cnt = 0;
+	for (int B = 1; B <= 500; B++)
+		for (int A = B; A <= 500; A++)
+			if (A * A - B * B == N) cnt++;
+
+	cout << cnt << endl;
+}
