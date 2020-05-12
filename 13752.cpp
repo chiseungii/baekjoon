a8c2789645b3395dd85ad27537d8a4f7e9f8fc6f
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-05-12 22:46:06 +0900

    Create 13752.cpp

---
+++ 13752.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int k; cin >> k;
+
+		for (int j = 0; j < k; j++)
+			cout << '=';
+		cout << endl;
+	}
+}
