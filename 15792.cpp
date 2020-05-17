600b51a77055f02d0eac59fed16552b971956f53
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-05-17 21:36:34 +0900

    Create 15792.cpp

---
+++ 15792.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int seat[1000][1000];
+
+int main() {
+	int A, B; cin >> A >> B;
+
+	cout << A / B << '.';
+	int tmp = A % B;
+	for (int i = 0; i < 1000; i++) {
+		tmp *= 10;
+		int a = tmp / B;
+		int b = tmp % B;
+		cout << a;
+
+		if (b == 0) break;
+		else tmp = b;
+	}
+	cout << '\n';
+}
