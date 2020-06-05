a46f918c16356910d8a906912dd9908712ce848a
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-06-05 18:02:06 +0900

    Create 04740.cpp

---
+++ 04740.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	while (1) {
+		string s; getline(cin, s);
+		if (s == "***") break;
+
+		for (int i = s.length() - 1; i >= 0; i--)
+			cout << s[i];
+		cout << endl;
+	}
+}
