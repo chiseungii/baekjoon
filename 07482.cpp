b09b8185593aa5492814b1b1c6895f7ded1e255b
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-06-08 20:55:07 +0900

    Create 07482.cpp

---
+++ 07482.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n; cin >> n;
+	
+	for (int i = 0; i < n; i++) {
+		double a; cin >> a;
+
+		cout << fixed;
+		cout.precision(10);
+		cout << a / 6 << endl;
+	}
+}
