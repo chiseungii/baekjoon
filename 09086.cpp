a5a0bda33474bbf35c7b52dd8933a6055b06b8b9
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-06-11 22:19:28 +0900

    Create 09086.cpp

---
+++ 09086.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T; cin >> T;
+
+	for (int i = 0; i < T; i++) {
+		string s; cin >> s;
+		cout << s[0] << s[s.length() - 1] << endl;
+	}
+}
