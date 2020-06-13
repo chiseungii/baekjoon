e61cb3e524b5b1f610313501f11291488f128168
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-06-13 21:50:21 +0900

    Create 10864.py

---
+++ 10864.py
@@ -0,0 +1,11 @@
+N, M = map(int, input().split())
+friends = [0] * (N + 1)
+
+for i in range(M):
+    A, B = map(int, input().split())
+
+    friends[A] += 1
+    friends[B] += 1
+
+for friend in friends[1:]:
+    print(friend)
