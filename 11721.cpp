d5e85374903894bdbce1bae727d10bc5a6f21e6d
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-06-15 18:42:57 +0900

    Create 11721.cpp

---
+++ 11721.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s; cin >> s;
+
+	int cnt = 0;
+	for (int i = 0; i < s.length(); i++) {
+		cout << s[i];
+		cnt++;
+
+		if (cnt == 10) {
+			cout << endl;
+			cnt = 0;
+		}
+	}
+}
