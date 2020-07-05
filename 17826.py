881f0813ef1aa0c9ac5e9e4c8ecb8cca4b685f45
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-07-05 23:14:24 +0900

    Create 17826.py

---
+++ 17826.py
@@ -0,0 +1,11 @@
+scores = list(map(int, input().split()))
+hongik = int(input())
+
+rank = scores.index(hongik) + 1
+if rank <= 5: print("A+")
+elif rank <= 15: print("A0")
+elif rank <= 30: print("B+")
+elif rank <= 35: print("B0")
+elif rank <= 45: print("C+")
+elif rank <= 48: print("C0")
+else: print("F")
