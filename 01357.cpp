066aa45bd05fa75f9c20fdce71000450884f16bb
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-07-13 22:47:30 +0900

    Create 01357.cpp

---
+++ 01357.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int Rev(int n) {
+	int result = 0;
+	while (n > 0) {
+		result *= 10;
+		result += n % 10;
+		n /= 10;
+	}
+
+	return result;
+}
+
+int main() {
+	int X, Y; cin >> X >> Y;
+	cout << Rev(Rev(X) + Rev(Y)) << endl;
+}
