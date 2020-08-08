5828eab854d6cb336fcef931c239b1ac95bfb76d
chiseungii <43616130+chiseungii@users.noreply.github.com>
2020-08-08 23:47:09 +0900

    Create 02083.py

---
+++ 02083.py
@@ -0,0 +1,8 @@
+while 1:
+    name, age, weight = input().split()
+    age, weight = int(age), int(weight)
+
+    if name == '#' and age == 0 and weight == 0: break
+
+    if age > 17 or weight >= 80: print(name, "Senior")
+    else: print(name, "Junior")
