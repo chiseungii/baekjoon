3bf674e2b3305af4c1d4054e25a7add06b3e84d4
tube <43616130+chiseungii@users.noreply.github.com>
2021-02-26 22:58:01 +0900

    Create 17219.py

---
+++ 17/17219.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+
+    passwords = {}
+
+    for i in range(N):
+        url, password = input().split()
+        passwords[url] = password
+
+    for i in range(M):
+        url = input()
+        print(passwords[url])
